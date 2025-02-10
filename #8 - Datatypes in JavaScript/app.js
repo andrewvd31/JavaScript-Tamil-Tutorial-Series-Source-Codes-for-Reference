9b5511b8f3d518720aac9f06de736da99a417a47
Andrew V D <andrewvd31@gmail.com>
2025-02-10 07:51:28 +0530

    Update v1.10

--- #8 - Datatypes in JavaScript/app.js
+++ #8 - Datatypes in JavaScript/app.js
@@ -1,3 +1,3 @@
-//imp! No code was practically executed for this section in the video
+//note: No code was practically executed for this section in the video
 
-//imp! Try testing some Javascript datatypes in this file.
+//note: Try testing some Javascript datatypes in this file.
