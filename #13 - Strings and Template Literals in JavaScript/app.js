cfe4fe7b7cecb09f844eae79c9083a932253b0c7
Andrew V D <andrewvd31@gmail.com>
2025-02-15 19:16:25 +0530

    Update v1.14

---
+++ #13 - Strings and Template Literals in JavaScript/app.js
@@ -0,0 +1,10 @@
+// note: Normal String Concatenation
+/*const demoVariable = 'Hello';
+  console.log("I'm" + hello + "from India");
+*/
+
+// note: Template Literals
+/* const demoVariable1 = 'Hello';
+console.log(`I am ${demoVariable1} from India`);
+*/
+
