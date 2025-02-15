b10748078f544a77bdfc6d4c095102069afaba5e
Andrew V D <andrewvd31@gmail.com>
2025-02-15 19:53:23 +0530

    Update v1.17

--- #13 - Strings and Template Literals in JavaScript/app.js
+++ #13 - Strings and Template Literals in JavaScript/app.js
@@ -5,9 +5,26 @@
 
 // note: Template Literals
 /* const demoVariable1 = 'Hello';
+const name1 = 56;
+const name2 = 90;
 console.log(`I am ${demoVariable1} from India`);
+console.log(`The value of ${name1 + name2} is 146`);
 */
 
-//note: Template Literals are available in JavaScript from ES6
-//note: Single quotes and double quotes won't work inside the template literal
+//note: Multi-line strings
+/* console.log('Hello \n\DemoName \n\lines') */
+/* soln: Hello 
+         DemoName 
+         lines */
+//imp!: This can be easily added with the help of template Literals
+console.log(`Hello
+DemoName
+lines`)
+/* soln: Hello 
+         DemoName 
+         lines */
+//imp!: For multiline strings, Always use template Literals
 
+//theory: Template Literals are available in JavaScript from ES6
+//theory: Single quotes and double quotes won't work inside the template literal
+//theory: We can also use backticks for any regular strings
