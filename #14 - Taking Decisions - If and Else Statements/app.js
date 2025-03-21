0332159a02172b8590fd1dd351dd4bfdcfb6c600
Andrew V D <andrewvd31@gmail.com>
2025-03-21 08:18:05 +0530

    Update v1.22

--- #14 - Taking Decisions - If and Else Statements/app.js
+++ #14 - Taking Decisions - If and Else Statements/app.js
@@ -1,9 +1,10 @@
-const age = 19
+const age = 15
 
 if (age >= 18){
     console.log('Yes. You can start driving a car');
 }
 
 else{
-    console.log('No. Yes. You cannot drive a car');
+    const years = 18 - age;
+    console.log(`No. You cannot drive a car. There are still ${years} years left for that`);
 }
