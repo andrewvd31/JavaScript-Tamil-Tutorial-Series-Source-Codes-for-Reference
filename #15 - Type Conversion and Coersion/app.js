af594be5652169c12acac7ac8450dce34ffb14a5
Andrew V D <andrewvd31@gmail.com>
2025-05-05 20:04:57 +0530

    Update v1.31

--- #15 - Type Conversion and Coersion/app.js
+++ #15 - Type Conversion and Coersion/app.js
@@ -1,6 +1,6 @@
 // note: Type Conversion - We manually convert types in JavaScript
 
-// note: Type Coersion - Browser automatically converts type in JavaScript
+// note: Type Coercion - Browser automatically converts type in JavaScript
 
 //note: Adding string with number gives concatenated string as result (Javascript automatically converts number to string datatype).
 
@@ -11,4 +11,6 @@
 //imp! The purple color in the browser denotes a number and the light blue number denotes a string.
 
 //note: We cannot convert something to undefined or null. We can only convert to a number, string or boolean.
+
+//note: We only rarely do Manual type conversion in JavaScript.
 
