25b090749cf8efadace2c93a1963b650bda8b191
Andrew V D <andrewvd31@gmail.com>
2025-05-05 19:56:07 +0530

    Update v1.28

--- #15 - Type Conversion and Coersion/app.js
+++ #15 - Type Conversion and Coersion/app.js
@@ -1,3 +1,8 @@
 // note: Type Conversion - We manually convert types in JavaScript
 
 // note: Type Coersion - Browser automatically converts type in JavaScript
+
+//note: Adding string with number gives concatenated string as result (Javascript automatically converts number to string datatype).
+
+//note: For Manual Type Conversion: Use Number(), String(), Boolean()
+
