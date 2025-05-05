b985be0e42fcd55a339ad3b71118230895553439
Andrew V D <andrewvd31@gmail.com>
2025-05-05 21:39:53 +0530

    Update v1.40

--- #17 - Equality and Strictly Equal Operator/app.js
+++ #17 - Equality and Strictly Equal Operator/app.js
@@ -1,1 +1,7 @@
 // note: == performs TYPE COERCION
+
+const user = prompt("Enter a number: ");
+// theory: typeof prompt() will be String.
+if (user === 5) console.log('Yes');
+//note: Not equal to:
+if (user !== 10) console.log('No');
