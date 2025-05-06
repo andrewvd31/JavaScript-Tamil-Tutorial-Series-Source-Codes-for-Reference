fd82e9318a4bdac9a63eea88529ca9f7709f447d
Andrew V D <andrewvd31@gmail.com>
2025-05-06 07:31:42 +0530

    Update v1.42

---
+++ #18 - The Boolean Logic/app.js
@@ -0,0 +1,7 @@
+//note: &&, || and ! Operators (Logical Operators)
+
+//imp! Boolean Logic is common for all programming languages
+
+//theory: AND Logical Operator returns true if both logics are true
+
+//theory: OR Logical Operator returns true if either one of the logic is true
