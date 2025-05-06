8c2017ebe8469acd288d4c56d1c6e3090892e95d
Andrew V D <andrewvd31@gmail.com>
2025-05-06 08:41:58 +0530

    Update v1.48

---
+++ #21 - Statements and Expressions/app.js
@@ -0,0 +1,5 @@
+//note: 5 + 7 is an expression because it evaluates to a value.
+
+//note: if(4 > 50) console.log("4 is greater than 50") is a statement because it does not evaluate to a value.
+
+//note: Anything that ends with a semicolon is a statement.
